0c6f079df12a7f08007e8fc62f4170531c9d1bf5
gfortin <gfortin@keyneosoft.fr>
2020-04-22 20:41:14 +0200

    Register to eureka server

--- src/main/kotlin/org/cloudchallenge/codex/security/SecurityApplication.kt
+++ src/main/kotlin/org/cloudchallenge/codex/security/SecurityApplication.kt
@@ -1,9 +1,13 @@
 package org.cloudchallenge.codex.security
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 class SecurityApplication
 
 fun main(args: Array<String>) {
