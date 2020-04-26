2cf6f61fb23dbe974d90466a71ff3dc5f53f7e03
gfortin <gfortin@keyneosoft.fr>
2020-04-26 12:00:17 +0200

    exclude netflix-ribbon dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,11 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("de.codecentric:spring-boot-admin-starter-client")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client"){
+		exclude(group = "org.springframework.cloud", module = "spring-cloud-starter-netflix-ribbon")
+		exclude(group = "org.springframework.cloud", module = "spring-cloud-netflix-ribbon")
+		exclude(group = "com.netflix.ribbon", module = "ribbon-eureka")
+	}
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
